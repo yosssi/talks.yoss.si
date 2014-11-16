14466eedcae6a925d2870f787a332cba3903c0c5
yosssi <yoshida.keiji.84@gmail.com>
2014-11-16 18:38:46 +0900

    Update 2015/0220-good-package.slide

--- 2015/0220-good-package/replace_global_var2.go
+++ 2015/0220-good-package/replace_global_var2.go
@@ -5,11 +5,11 @@ import "os"
 
 // START OMIT
 
-var exit = os.Exit // HL
+var exit = os.Exit // Define a global variable. // HL
 
 func main() {
 	if len(os.Args) < 2 {
-		exit(1) // HL
+		exit(1) // Can test this block by replacing the value of the exit function. // HL
 	}
 }
 
---
+++ 2015/0220-good-package/table_driven_tests_test.go
@@ -0,0 +1,27 @@
+// +build ignore
+package main
+
+import "testing"
+
+// START OMIT
+
+var testCases = []struct {
+	in  string
+	out string
+}{
+	{"a", "A"},
+	{"b", "B"},
+	{"c", "C"},
+	{"d", "D"},
+	{"e", "E"},
+}
+
+func TestToUpper(t *testing.T) {
+	for _, tc := range testCases {
+		if result := ToUpper(tc.in); result != out {
+			t.Errorf("ToUpper(%q) => %q, want %q", tc.in, result, tt.out)
+		}
+	}
+}
+
+// END OMIT
