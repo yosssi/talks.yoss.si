174a97f9b18298dc3687da9bfcb3899de02de3eb
yosssi <yoshida.keiji.84@gmail.com>
2014-11-22 14:04:25 +0900

    Update 2015/0220-good-package.slide

--- 2015/0220-good-package/export_identifiers_ok.go
+++ 2015/0220-good-package/export_identifiers_ok.go
@@ -3,7 +3,7 @@ package main
 
 // START OMIT
 
-// PrivateStruct is used only in this package.
+// privateStruct is used only in this package.
 type privateStruct struct{} // HL
 
 // PublicFunc is used not only in this package but also other packages.
